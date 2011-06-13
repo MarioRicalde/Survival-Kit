d2ad8ea19076acaca67671f92ff68f0c0bf00838
Mario Kuroir Ricalde <kuroir@gmail.com>
2011-06-13 09:22:07 -0500

    Some reorganization

---
+++ config.rb
@@ -0,0 +1,14 @@
+# Set this to the root of your project when deployed:
+project_type      = :stand_alone
+
+http_path         = "/"
+
+css_dir           = "stylesheets/"
+sass_dir          = "src/"
+images_dir        = "images/"
+javascripts_dir   = "js"
+# http_images_path  = "/assets/"
+
+line_comments     = false
+relative_assets   = true
+output_style      = :compressed  # :nested, :expanded, :compact, or :compressed.
---
+++ extras/readme.markdown
@@ -0,0 +1,1 @@
+I love HTML5Boilerplate.
