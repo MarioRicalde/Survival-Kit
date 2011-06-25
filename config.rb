8e1fc35244e5c32b9fa38081025416cdd6755fad
Mario Kuroir Ricalde <kuroir@gmail.com>
2011-06-25 02:52:05 -0500

    Including CoffeeScript compilation utilities and Major refactor.

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ project_type      = :stand_alone
 http_path         = "/"
 
 css_dir           = "stylesheets/"
-sass_dir          = "src/"
+sass_dir          = "src/stylesheets"
 images_dir        = "images/"
-javascripts_dir   = "js"
+javascripts_dir   = "javascripts"
 # http_images_path  = "/assets/"
 
 line_comments     = false
 relative_assets   = true
-output_style      = :compressed  # :nested, :expanded, :compact, or :compressed.
+output_style      = :expanded  # :nested, :expanded, :compact, or :compressed.
---
+++ src/javascripts/core.coffee
@@ -0,0 +1,5 @@
+class Core
+
+    constructor: ->
+        console.log 'Just a placeholder.'
+    
